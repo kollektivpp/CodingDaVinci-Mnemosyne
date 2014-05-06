411b283c11db2eccb316be74ca0ffcfade9fb31c
Thomas Fett <user@thomasfett.de>
2014-05-07 00:07:18 +0200

    Created angular app with yo generator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('mnemosyneApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
