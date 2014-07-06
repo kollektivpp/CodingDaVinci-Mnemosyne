3c7a9242b4dcead94ed7ac928310d16555a29c64
Thomas Fett <user@thomasfett.de>
2014-07-06 02:16:24 +0200

    Fixed regex replacements for wiki data

--- app/scripts/viewmodel/personNode.js
+++ app/scripts/viewmodel/personNode.js
@@ -20,23 +20,33 @@ angular.module('mnemosyneApp').factory('PersonNode', function () {
         this.wiki = data.wiki;
 
         if (this.wiki) {
-            this.wiki.content.forEach( function(element) {
-                element.html = element.html.replace(/<[^>]*>/g, "");
-                element.html = element.html.replace(/.*\[Bearbeiten\]/g, "");
-                element.html = element.html.replace(/Einzelnachweisfehler.*/g, "");
-                element.html = element.html.replace(/Referenzfehler.*/g, "");
-                element.html = element.html.replace(/Hinweis:.*/g, "");
+            this.wiki.html = this.wiki.html.replace(/<[^>]*>/g, "");
+            this.wiki.html = this.wiki.html.replace(/.*\[Bearbeiten\]/g, "");
+            this.wiki.html = this.wiki.html.replace(/Einzelnachweisfehler.*/g, "");
+            this.wiki.html = this.wiki.html.replace(/Referenzfehler.*/g, "");
+            this.wiki.html = this.wiki.html.replace(/Hinweis:.*/g, "");
+            
+            if (this.wiki.content) {
+                this.wiki.content.forEach( function(element) {
+                    if (element.html) {
+                        element.html = element.html.replace(/<[^>]*>/g, "");
+                        element.html = element.html.replace(/.*\[Bearbeiten\]/g, "");
+                        element.html = element.html.replace(/Einzelnachweisfehler.*/g, "");
+                        element.html = element.html.replace(/Referenzfehler.*/g, "");
+                        element.html = element.html.replace(/Hinweis:.*/g, "");
 
-                if (element.content) {
-                    element.content.forEach( function(innerElement) {
-                        innerElement.html = innerElement.html.replace(/<[^>]*>/g, "");
-                        innerElement.html = innerElement.html.replace(/.*\[Bearbeiten\]/g, "");
-                        innerElement.html = innerElement.html.replace(/Einzelnachweisfehler.*/g, "");
-                        innerElement.html = innerElement.html.replace(/Referenzfehler.*/g, "");
-                        innerElement.html = innerElement.html.replace(/Hinweis:.*/g, "");
-                    });
-                }
-            });
+                        if (element.content) {
+                            element.content.forEach( function(innerElement) {
+                                innerElement.html = innerElement.html.replace(/<[^>]*>/g, "");
+                                innerElement.html = innerElement.html.replace(/.*\[Bearbeiten\]/g, "");
+                                innerElement.html = innerElement.html.replace(/Einzelnachweisfehler.*/g, "");
+                                innerElement.html = innerElement.html.replace(/Referenzfehler.*/g, "");
+                                innerElement.html = innerElement.html.replace(/Hinweis:.*/g, "");
+                            });
+                        }
+                    }
+                });
+            }
         }
 
         this.thumbnail = data.thumbnail;
