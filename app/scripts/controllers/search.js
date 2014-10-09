fbc6f2d523ce337ddf6aabb6695dd3db92eb38ab
Knut Perseke <knut.perseke@gmail.com>
2014-10-09 06:58:21 +0200

    completely restructured the way of controlling the tool using the hardwarebox, new workflow to search, added scrolling to more fields

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -14,9 +14,16 @@ angular.module('mnemosyneApp')
             $scope.encoderValue = 0;
             $scope.lastValue = 0;
 
-            $scope.lastMore = -1;
             $scope.shareActivated = false;
             $scope.hasResults = true;
+            $scope.scroll = 0;
+            $scope.recentScrollData = 0;
+
+            //focus fields
+            $scope.ShareToFocus = false;
+            $scope.ShareFromFocus = false;
+            $scope.ShareAdditionalTextFocus = false;
+            $scope.ShareSendFocus = false;
 
             console.log("initialized searchterm");
 
@@ -79,7 +86,7 @@ angular.module('mnemosyneApp')
                     overlay = $('#search-overlay');
 
                 if (searchButton.hasClass('active')) {
-
+                    console.log("search was active");
                     if ($('.searchTerm').val() !== '') {
                         $scope.searchTerm.value = '';
                         $scope.triggerSearch();
@@ -91,6 +98,25 @@ angular.module('mnemosyneApp')
                     $('.random-search-overlay').removeClass('selected');
                     $('.search-search-overlay').addClass('selected');
                     $('.searchTerm').focus();
+                }
+            };
+
+            $scope.changeSearchOptionVisibity = function() {
+                var searchButton = $('#button-search');
+                var overlay = $('#search-overlay');
+                if (searchButton.hasClass('active')) {
+                    console.log('search was active, will be removed');
+                    overlay.addClass('hidden');
+                    searchButton.removeClass('active');
+                    searchButton.html("RESTART");
+                }  else {
+                    overlay.removeClass('hidden');
+                    searchButton.addClass('active');
+                    $('.searchTerm').focus();
+                    searchButton.html("GO");
+                    if ($scope.shareActivated) {
+                        $scope.sharePressed();
+                    }
                 }
             };
 
@@ -268,6 +294,39 @@ angular.module('mnemosyneApp')
             }
         }
 
+        $scope.transforToGermanKey = function(key) {
+            switch (key) {
+                case 'z' :
+                    key = 'y'
+                    break;
+                case 'y' :
+                    key = 'z'
+                    break;
+                case '-' :
+                    key = 'ß'
+                    break;
+                case '\'' :
+                    key = 'ä'
+                    break;
+                case '"' :
+                    key = 'Ä'
+                    break;
+                case ';' :
+                    key = 'ö'
+                    break;
+                case ':' :
+                    key = 'Ö'
+                    break;
+                case '[' :
+                    key = 'ü'
+                    break;
+                case '{' :
+                    key = 'Ü'
+                    break;
+            }
+            return key;
+        }
+
         $scope.toggleSwitch = function(direction) {
             if ($('#search-overlay:visible').length !== 0) {
                 if (direction > 0) {
@@ -283,121 +342,232 @@ angular.module('mnemosyneApp')
             }
         };
 
-        if(socket.on) {
-            socket.on('controlstation', function(data) {
+        //pargma mark hardware buttons
+        $scope.keyboardPressed = function(key) {
+            console.log("detected key");
+            var currentText = angular.element(document.querySelector('.overlay-search-selector input')).val();
+            
+            if (key === 127) {
+                currentText = currentText.substring(0, currentText.length - 1);
+            } 
+            else {
+                currentText += $scope.transforToGermanKey(String.fromCharCode(key));
+            }
+            angular.element(document.querySelector('.overlay-search-selector input')).val(currentText);
+        };
 
-                if (data === 1000) {
-                    $scope.toggleMore(1);
-                }
-
-                if (data === 2000) {
-                    $scope.toggleMore(2);
-                }
-
-                if (data === 3000) {
-                    $scope.toggleMore(3);
-                }
-
-                if (data === 4000) {
-                    $scope.toggleMore(4);
-                }
-
-                if (data === 5000) {
-                    $scope.toggleMore(5);
-                }
-
-                if (data === 6000) {
-                    $scope.shareActivated = !$scope.shareActivated;
-                    console.log("Activated: " + $scope.shareActivated);
-                    return $scope.standardButtonClick({
-                        srcElement: document.querySelector('.share')
-                    });
-                } else if (data === 7000) {
-                    if ($('.meta-view').length === 0) {
-                        return $scope.openMetaView();
-                    }
-
-                    return $location.path('/search');
-
-                } else if (data === 8000) {
-                    $scope.searchButtonClick({
-                        srcElement: document.querySelector('.restart')
-                    });
-                    return $scope.standardButtonClick({
-                        srcElement: document.querySelector('.restart')
-                    });
-                }
-
-                /**
-                 * if search and random overlays are open dont modify encode
-                 */
-                if ($('#search-overlay:visible').hasClass('hidden') === false) {
-                    if ($scope.lastValue > data) {
-                        $scope.encoderValue--;
-                    } else {
-                        $scope.encoderValue++;
-                    }
-
-                    if (Math.abs($scope.encoderValue) >= 50) {
-                        if ($scope.encoderValue < 0) {
-                            $scope.toggleSwitch(-1);
-                            $scope.encoderValue = -50;
-                        } else {
-                            $scope.toggleSwitch(1);
-                            $scope.encoderValue = 50;
-                        }
-                    }
-
-                    /**
-                     * when encoder click was fired
-                     */
-                    if (data === 9000) {
-                        $scope.searchButtonClick({
-                            srcElement: document.querySelector('.restart')
-                        });
-                        $scope.standardButtonClick({
-                            srcElement: document.querySelector('.restart')
-                        });
-                    }
-
-                    return $scope.lastValue = data;
-                } else if ($scope.shareActivated) {
-                    var inputs = document.getElementById("share-overlay").children;
-                    console.log(inputs);
-                    $('#share-overlay').children()[0].focus();
-                    // if (data >= -210 && data <= -150) {
-                    //     console.log("first focus");
-                    //     inputs[0].focus();
-                    // } else if (data >= -149 && data <= -100) {
-                    //     console.log("second focus");
-                    //     inputs[1].focus();
-                    // } else if (data >= -99 && data <= -50) {
-                    //     console.log("third focus");
-                    //     inputs[2].focus();
-                    // } else if (data >= -49) {
-                    //     console.log("fourth focus");
-                    //     inputs[3].focus();
-                    // }
-                }   
-                else {
-
-                    if (data >= -210 && data <= -150) {
-                        $scope.requestDepth = 1;
-                    } else if (data >= -149 && data <= -100) {
-                        $scope.requestDepth = 2;
-                    } else if (data >= -99 && data <= -50) {
-                        $scope.requestDepth = 3;
-                    } else if (data >= -49 && data <= 0) {
-                        $scope.requestDepth = 4;
-                    } else if (data > 0 && data <= 50) {
-                        $scope.requestDepth = 5;
-                    }
-
-                }
-
-                $scope.displaySearchDepth();
-
-            });
+        $scope.enterPressed = function() {
+            $scope.searchButtonClick();
         }
 
+        $scope.sharePressed = function() {
+            $scope.shareActivated = !$scope.shareActivated;
+            console.log("Activated: " + $scope.shareActivated);
+            $scope.standardButtonClick({
+                        srcElement: document.querySelector('.share')
+                });
+        }
+
+        $scope.metaPressed = function() {
+            if ($('.meta-view').length === 0) {
+                $scope.openMetaView();
+            } 
+            else {
+               $location.path('/search'); 
+            }            
+        }
+
+        $scope.restartPressed = function() {
+            $scope.changeSearchOptionVisibity();
+        }
+
+        $scope.potiPressed = function(data) {
+            console.log("poti pressed");
+            var searchStyleElements = angular.element(document.querySelectorAll('.overlay-search-selector'));
+            var clickedElement = (angular.element(event.srcElement).hasClass('.overlay-search-selector')) ? angular.element(event.srcElement) : angular.element(event.srcElement).parent();
+            
+            if ($('.random-search-overlay').hasClass('selected')) {
+                $scope.potiPressedRandomSearchAction();
+                return;
+            }
+
+            if ($('.search-search-overlay').hasClass('selected')) {
+                $scope.triggerSearch();
+                return
+            }
+                
+        }
+
+        $scope.potiChanged = function(data) {
+            var searchButton = angular.element(document.querySelector('#button-search'));
+            var moreElements = $('.moreIsShown');
+            if (searchButton.hasClass('active')) {
+                $scope.potiChangedSearchAction(data);
+                return;
+            } 
+            if (moreElements.length > 0) {
+                $scope.potiChangedScrollMoreAction(moreElements[0], data);
+                return;
+            }
+            if ($scope.shareActivated) {
+                $scope.potiChangedShareActivatedAction(data);
+                return;
+            }
+            
+            $scope.potiChangedDisplaySearchAction(data);  
+            
+        }
+
+        $scope.potiChangedSearchAction = function(data) {
+            if (data <= -120) {
+                    $('.search-search-overlay').removeClass('selected');
+                    $('.random-search-overlay').addClass('selected');
+                }
+                else {
+                    $('.search-search-overlay').addClass('selected');
+                    $('.random-search-overlay').removeClass('selected');
+                }
+        }
+
+        $scope.potiChangedDisplaySearchAction = function(data) {
+            console.log("potiChangedDisplaySearchAction");
+            if (data >= -210 && data <= -150) {
+                $scope.requestDepth = 1;
+            } else if (data >= -149 && data <= -100) {
+                $scope.requestDepth = 2;
+            } else if (data >= -99 && data <= -50) {
+                $scope.requestDepth = 3;
+            } else if (data >= -49 && data <= 0) {
+                $scope.requestDepth = 4;
+            } else if (data > 0 && data <= 50) {
+                $scope.requestDepth = 5;
+            }
+
+            $scope.displaySearchDepth();
+        }
+
+        $scope.potiChangedScrollMoreAction = function(element, data) {
+            console.log("recentScrollData");
+            console.log($scope.scroll);
+            data = data > 50 ? 50 : data;
+            data = data < -200 ? -200 : data;
+            
+            if ($scope.recentScrollData <= data)
+                $scope.scroll += 50;
+            if ($scope.recentScrollData >= data)
+                ($scope.scroll -= 50);
+            $('.moreIsShown.moreWiki').animate({scrollTop: $scope.scroll}, 0);
+            $scope.recentScrollData = data;
+        }
+
+        $scope.potiPressedRandomSearchAction = function() {
+            console.log("PotiPressedRandomSearchAction");
+            angular.element(document.querySelector('.overlay-search-selector input')).val($scope.randomSearchString());
+
+        }
+
+        $scope.potiChangedShareActivatedAction = function(data) {
+            var inputs = document.getElementById("share-overlay").children;
+            //console.log(inputs);
+            $('#share-overlay').children()[0].focus();
+             if (data >= -210 && data <= -150) {
+                 console.log("first focus");
+                 //console.log(inputs[0]);
+                 $scope.ShareToFocus = true;
+                 //inputs[0].focus();
+             } else if (data >= -149 && data <= -100) {
+                 console.log("second focus");
+                 $scope.ShareFromFocus = true;
+                 //inputs[1].focus();
+             } else if (data >= -99 && data <= -50) {
+                 console.log("third focus");
+                 $scope.ShareAdditionalTextFocus = true;
+                 inputs[2].focus();
+             } else if (data >= -49) {
+                 console.log("fourth focus");
+                 $scope.ShareSendFocus = true;
+                 inputs[3].focus();
+
+            }
+        }
+
+        if(socket.on) {
+            socket.on('controlstation', function(data) {
+                switch(true) {
+                    case (data >= 100 && data <= 227) :
+                        $scope.keyboardPressed(data - 100);
+                        break;
+                    case (data === 230) :
+                        $scope.enterPressed();
+                        break;
+                    case (data === 1000) :
+                        $scope.toggleMore(1);
+                        break;
+                    case (data === 2000) :
+                        $scope.toggleMore(2);
+                        break;
+                    case (data === 3000) :
+                        $scope.toggleMore(3);
+                        break;
+                    case (data === 4000) :
+                        $scope.toggleMore(4);
+                        break;
+                    case (data === 5000) :
+                        $scope.toggleMore(5);
+                        break;
+                    case (data === 6000) :
+                        $scope.sharePressed();
+                        break;
+                    case (data === 7000) :
+                        $scope.metaPressed();
+                        break;
+                    case (data === 8000) :
+                        $scope.restartPressed(); 
+                        break;   
+                    case (data === 9000) :
+                        $scope.potiPressed(data);
+                        break;
+                    default:
+                        $scope.potiChanged(data);
+                        break;
+                
+                }
+ 
+            });
+        }
 });
+
+/**
+                 * if search and random overlays are open dont modify encode
+                 */
+                // if ($('#search-overlay:visible').hasClass('hidden') === false) {
+                //     if ($scope.lastValue > data) {
+                //         $scope.encoderValue--;
+                //     } else {
+                //         $scope.encoderValue++;
+                //     }
+
+                //     if (Math.abs($scope.encoderValue) >= 50) {
+                //         if ($scope.encoderValue < 0) {
+                //             $scope.toggleSwitch(-1);
+                //             $scope.encoderValue = -50;
+                //         } else {
+                //             $scope.toggleSwitch(1);
+                //             $scope.encoderValue = 50;
+                //         }
+                //     }
+                // }
+
+
+                /**
+                 * when encoder click was fired
+                 */
+
+                    // $scope.searchButtonClick({
+                    //     srcElement: document.querySelector('.restart')
+                    // });
+                    // $scope.standardButtonClick({
+                    //     srcElement: document.querySelector('.restart')
+                    // });
+                    
+
