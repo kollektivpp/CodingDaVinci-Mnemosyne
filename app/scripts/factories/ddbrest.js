9826a6d9d04bb1f2abcd1478f15457c370a1cf30
christian-bromann <contact@christian-bromann.com>
2015-11-28 12:08:55 +0100

    only do secure requests

--- app/scripts/factories/ddbrest.js
+++ app/scripts/factories/ddbrest.js
@@ -1,5 +1,5 @@
 'use strict';
 
 angular.module('mnemosyneApp').factory('DDBRest', function () {
-    return new DDBRest('uX3gw0Nce6EQpHqxdmEXjny4mty4BNWd5ELnyurt1qL5GRcRu0I1398599442766');
+    return new DDBRest('uX3gw0Nce6EQpHqxdmEXjny4mty4BNWd5ELnyurt1qL5GRcRu0I1398599442766', true);
 });
