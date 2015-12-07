b7bcf8b2953fd7be3e39f8a404f8d60dc8a09609
Knut Perseke <knut.perseke@gmail.com>
2015-12-07 15:55:23 +0100

    add attribution loading to resultThread instead of ResultParser

--- app/scripts/services/requestThread.js
+++ app/scripts/services/requestThread.js
@@ -29,6 +29,16 @@ angular.module('mnemosyneApp').service('RequestThread', function ($http, Request
             console.log('All requests have finished');
             //if (this.requestResults[this.requestResults.length - 1].outcome.type === "PERSON") {
             var wikiPromise = WikiParser.getWikiData(self.requestResults[this.requestResults.length - 1].outcome.title);
+            if (this.requestResults[this.requestResults.length - 1].outcome.type === "PERSON") {
+              var item = this.requestResults[this.requestResults.length - 1].outcome;
+                var thumbnailData = getAttributionForThumbnail(item.thumbnail);
+                if (thumbnailData) {
+                  item.attribution = thumbnailData;
+                }
+                else {
+                  item.thumbnail = undefined;
+                }
+              }
             wikiPromise.then(function(data) {
                 console.log("Loaded Toc successfully");
                 console.log(data);
@@ -42,6 +52,32 @@ angular.module('mnemosyneApp').service('RequestThread', function ($http, Request
                 EventSystem.dispatchEvent(self.finishedEvent);
             })
             return;
+        }
+
+        function getAttributionForThumbnail(thumbnailUrl) {
+            if (thumbnailUrl.match('commons.wikimedia.org')) {
+              //Image is from commons
+               var attribution = {};
+               var commonsFileName = thumbnailUrl.match('[a-zA-z0-9]*\.jpg');
+               var url = 'https://en.wikipedia.org/w/api.php?action=query&prop=imageinfo&iiprop=extmetadata&titles=File%3a' +
+                 commonsFileName + '&format=json&callback=JSON_CALLBACK';
+              $http.jsonp(url)
+                .success(function(response) {
+                  console.log('loaded from commons');
+                  var metadata = response.query.pages['-1'].imageinfo[0].extmetadata;
+                  attribution.credit = metadata.Credit.value;
+                  attribution.author = metadata.Artist.value;
+                  attribution.license = metadata.LicenseShortName.value;
+                  attribution.addon = 'via Wikimedia Commons';
+                  attribution.commonsUrl = 'https://commons.wikimedia.org/wiki/File:' + commonsFileName;
+                })
+                .error(function(response, status, headers, config) {
+                  console.log('error from commons');
+                  console.log(response);
+                });
+              return attribution;
+            }
+            return undefined;
         }
 
         DDBRest.search(searchTerm).affiliate().place().get(0, 50).then(function(data) {
--- app/scripts/services/resultParser.js
+++ app/scripts/services/resultParser.js
@@ -4,33 +4,9 @@ angular.module('mnemosyneApp').service('ResultParser', function ($http, RequestB
 
     function ResultParser(responseData) {
         this.responseData = responseData;
-    }
+    };
 
-    function getAttributionForThumbnail(thumbnailUrl) {
-        if (thumbnailUrl.match('commons.wikimedia.org')) {
-          //Image is from commons
-          var attribution = {};
-          var commonsFileName = thumbnailUrl.match('[a-zA-z0-9]*\.jpg');
-          var url = 'https://en.wikipedia.org/w/api.php?action=query&prop=imageinfo&iiprop=extmetadata&titles=File%3a' +
-            commonsFileName + '&format=json&callback=JSON_CALLBACK';
-          $http.jsonp(url)
-            .success(function(response) {
-              console.log('loaded from commons');
-              var metadata = response.query.pages['-1'].imageinfo[0].extmetadata;
-              attribution.credit = metadata.Credit.value;
-              attribution.author = metadata.Artist.value;
-              attribution.license = metadata.LicenseShortName.value;
-              attribution.addon = 'via Wikimedia Commons';
-              attribution.commonsUrl = 'https://commons.wikimedia.org/wiki/File:' + commonsFileName;
-            })
-            .error(function(response, status, headers, config) {
-              console.log('error from commons');
-              console.log(response);
-            });
-          return attribution;
-        }
-        return undefined;
-    }
+
 
     ResultParser.prototype.createOutcome = function() {
         var resultObject = {};
@@ -93,19 +69,19 @@ angular.module('mnemosyneApp').service('ResultParser', function ($http, RequestB
         console.log(entityData);
 
         if (entityData !== undefined) {
-            resultObject.title = entityData.preferredName;
+            resultObject.title = entityData.preferredName.replace(/<[\/]*match>/g, "");
             resultObject.dateOfBirth = entityData.dateOfBirth;
             resultObject.dateOfDeath = entityData.dateOfDeath;
             resultObject.placeOfBirth = entityData.placeOfBirth;
             resultObject.placeOfDeath = entityData.placeOfDeath;
-
-            if (entityData.thumbnail) {
-              var thumbnailData = getAttributionForThumbnail(entityData.thumbnail);
-              if (thumbnailData) {
-                resultObject.thumbnail = entityData.thumbnail;
-                resultObject.attribution = thumbnailData;
-              }
-            }
+            resultObject.thumbnail = entityData.thumbnail;
+            // if (entityData.thumbnail) {
+            //   var thumbnailData = getAttributionForThumbnail(entityData.thumbnail);
+            //   if (thumbnailData) {
+            //     resultObject.thumbnail = entityData.thumbnail;
+            //     resultObject.attribution = thumbnailData;
+            //   }
+            // }
             resultObject.variantName = entityData.variantName;
             resultObject.professionOrOccupation = entityData.professionOrOccupation;
         }
